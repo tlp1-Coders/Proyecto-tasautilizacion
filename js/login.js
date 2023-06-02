f0654654ed03f028aedacfd2a93b1b87a04c683e
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2023-06-02 15:24:16 -0300

    asd

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const formulario= document.getElementById('ingresar');
+
+formulario.addEventListener('click',()=>{
+    
+    let email=document.getElementById('email');
+    let user=document.getElementById('user');
+    let password=document.getElementById('password');
+})
