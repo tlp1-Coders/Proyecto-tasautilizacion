020e6113193e6adba9a16aae2e0358c1af0461e1
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2023-06-05 12:01:12 -0300

    asd

--- js/login.js
+++ js/login.js
@@ -1,10 +1,23 @@
 const formulario= document.getElementById('incioSesion');
-const  cuenta={
-    email:'augustochavesfiore@gmail.com',
-    user:'augusto',
-    password:'password'
+const  cuenta=[
+    {
+        email:'augustochavesfiore@gmail.com',
+        user:'augusto',
+        password:'password'
+    },
+    {
+        email:'garciafabianemmanuel@gmail.com',
+        user:'fabian',
+        password:'password'
+    },
+    {
+        email:'canepamariajose95@gmail.com',
+        user:'majo',
+        password:'password'
+    },
+    
 
-}
+]
 
 const $form = document.querySelector('form')
 
@@ -13,15 +26,14 @@ formulario.addEventListener('click',()=>{
     let email=document.getElementById('email').value;
     let user=document.getElementById('user').value;
     let password=document.getElementById('password').value;
-    
-    if(email===cuenta.email && user===cuenta.user && password===cuenta.password){
-        alert('dentro');
-        $form.setAttribute('action','consulta.html')
-    }    
-    else{
+    const validacion=  cuenta.find(e=>email===e.email && user===e.user && password===e.password);     
+    if(!validacion){
         alert("Cuenta incorrecta")
-
-    }
+        return
+    } 
+    alert('dentro');
+    $form.setAttribute('action','consulta.html')
+        
 })
 
 console.log(email.value);
