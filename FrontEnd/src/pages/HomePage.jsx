7b987d3252ad6929710a5becabe1aa115e77ce71
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2023-11-23 16:17:11 -0300

    Seccion de Home

--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { Container, Grid, Typography, Paper, Box } from '@mui/material';
+
 
 export const HomePage = () => {
   return (
-    <>
-      <div className="container d-flex justify-content-center">
-        <div className="row">
-          <div className="col-12">
-            <h1>Bienvenido</h1>
-          </div>
-          <div className="col-12 row">
-            <main className='col-7'>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio facilis, quibusdam quas excepturi minus dolorum. Nihil facere repudiandae assumenda ipsum magni sint molestias dignissimos unde consequatur, impedit officia voluptas qui.</p>
-            </main>
-            <aside className='col-5'>
-                <img className='img-fluid rounded' src="../../public/pagatuscuentas.png" alt="paga tus cuentas" />
-            </aside>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+    <Container
+      component="main"
+      maxWidth="md"
+    sx={{ 
+      backgroundColor: "white",
+      opacity: 0.9,
+      borderRadius: "10px",
+      boxShadow:15,
+      textAlign: "center",
+  }}
+    >
+      <Grid sx={{height:"80vh"}} container justifyContent="center" alignItems="center">
+        <Grid item xs={12}>
+          <Typography variant="h1">Bienvenidos a Fermosa</Typography>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={7}>
+            <Paper elevation={5}>
+              <Box p={2}>
+                <Typography variant="body1">
+                  Buscamos facilitarte tus trámites burocráticos asociados a la Municipalidad de Formosa.
+                  Hoy en día ya está disponible la sección de consulta y pago de la tasa de utilización de la vía.
+                  Registrate y comenzá a disfrutar de los beneficios!
+                </Typography>
+                <Typography variant="body1">
+                  Próximamente estaremos agregando más servicios para que puedas realizar tus trámites de manera online.
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={5}>
+              <Box>
+                <img src="../../public/pagatuscuentas.png" alt="paga tus cuentas" className='img-fluid' />
+              </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
